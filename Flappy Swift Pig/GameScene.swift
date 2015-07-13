27131025cc6180ca89a30a43f4d98ecffde17d02
Konstantin Tchernov <k.tchernov@gmail.com>
2015-07-13 20:56:38 +1200

    Complete basic game

--- Flappy Swift Pig/GameScene.swift
+++ Flappy Swift Pig/GameScene.swift
@@ -8,38 +8,98 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var pig = SKNode()
+    
+    var pipeUpTexture = SKTexture()
+    var pipeDownTexture = SKTexture()
+    var PipesMoveAndRemove = SKAction()
+    
+    let kPipeGap = 170.0
+    
+    let kPigCategory = UInt32(1 << 2)
+    
+    
+    var gameOver = false
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        pig = childNodeWithName("PigNode")!
         
-        self.addChild(myLabel)
+        pig.physicsBody!.categoryBitMask = kPigCategory
+        
+        self.physicsWorld.gravity = CGVectorMake(0, -7.0)
+        self.physicsWorld.contactDelegate = self
+        
+        pipeUpTexture = SKTexture(imageNamed:"PipeUp")
+        pipeDownTexture = SKTexture(imageNamed:"PipeDown")
+        
+        // movement of pipes
+        
+        let distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeUpTexture.size().width)
+        let movePipes = SKAction.moveByX(-distanceToMove, y: 0.0, duration: NSTimeInterval(0.01 * distanceToMove))
+        let removePipes = SKAction.removeFromParent()
+        
+        PipesMoveAndRemove = SKAction.sequence([movePipes,removePipes])
+        
+        //Spawn Pipes
+        
+        let spawn  = SKAction.runBlock({() in self.spawnPipes()})
+        let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        self.runAction(spawnThenDelayForever, withKey:"SpawnPipesAction")
+    }
+    
+    func spawnPipes() {
+        
+        let pipePair = SKNode()
+        pipePair.position = CGPointMake(self.frame.size.width + pipeUpTexture.size().width * 2, 0)
+        pipePair.zPosition = -10
+        
+        let height = UInt32(self.frame.size.height / 6)
+        let y = arc4random() % height + height
+        
+        let pipeDown = SKSpriteNode(texture: pipeDownTexture)
+        pipeDown.setScale(2.0)
+        pipeDown.position = CGPointMake(0.0, CGFloat(y) + pipeDown.size.height + CGFloat(kPipeGap))
+        
+        pipeDown.physicsBody = SKPhysicsBody(rectangleOfSize:pipeDown.size)
+        pipeDown.physicsBody!.dynamic = false
+        pipeDown.physicsBody!.contactTestBitMask = kPigCategory
+        pipePair.addChild(pipeDown)
+        
+        let pipeUp = SKSpriteNode(texture: pipeUpTexture)
+        pipeUp.setScale(2.0)
+        pipeUp.position = CGPointMake(0.0, CGFloat(y))
+        
+        pipeUp.physicsBody = SKPhysicsBody(rectangleOfSize: pipeUp.size)
+        pipeUp.physicsBody!.dynamic = false
+        pipeUp.physicsBody!.contactTestBitMask = kPigCategory
+        pipePair.addChild(pipeUp)
+        
+        pipePair.runAction(PipesMoveAndRemove, withKey:"PipesMoveAction")
+        self.addChild(pipePair)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        if (!gameOver) {
+            pig.physicsBody!.velocity = CGVectorMake(0, 0)
+            pig.physicsBody!.applyImpulse(CGVectorMake(0, 50))
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        if (!gameOver) {
+            gameOver = true
+            pig.physicsBody!.collisionBitMask = 0
+            pig.physicsBody!.velocity = CGVectorMake(0, -20)
+
+            removeAllActions()
+        }
     }
 }
